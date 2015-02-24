a6d1104212f280e818d63d025ce9925de8a446e0
Mohamed Ahmed Abdel Fattah <mohamedahmedabdelfattah@gmail.com>
2015-02-24 17:31:20 +0200

    Initial Commit

---
+++ 101Encryption/MainFrm.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace _101Encryption
+{
+    partial class MainFrm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainFrm));
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.lblInput = new System.Windows.Forms.Label();
+            this.lblKey = new System.Windows.Forms.Label();
+            this.txtKey = new System.Windows.Forms.TextBox();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.ForeColor = System.Drawing.Color.Red;
+            this.btnEncrypt.Location = new System.Drawing.Point(175, 73);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(87, 27);
+            this.btnEncrypt.TabIndex = 3;
+            this.btnEncrypt.Text = "Encrypt";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(135, 15);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(286, 23);
+            this.txtInput.TabIndex = 1;
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnDecrypt.Location = new System.Drawing.Point(268, 73);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(87, 27);
+            this.btnDecrypt.TabIndex = 4;
+            this.btnDecrypt.Text = "Decrypt";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(135, 106);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ReadOnly = true;
+            this.txtOutput.Size = new System.Drawing.Size(286, 23);
+            this.txtOutput.TabIndex = 3;
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Location = new System.Drawing.Point(81, 109);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(48, 15);
+            this.lblOutput.TabIndex = 4;
+            this.lblOutput.Text = "Output";
+            // 
+            // lblInput
+            // 
+            this.lblInput.AutoSize = true;
+            this.lblInput.Location = new System.Drawing.Point(81, 18);
+            this.lblInput.Name = "lblInput";
+            this.lblInput.Size = new System.Drawing.Size(38, 15);
+            this.lblInput.TabIndex = 5;
+            this.lblInput.Text = "Input";
+            // 
+            // lblKey
+            // 
+            this.lblKey.AutoSize = true;
+            this.lblKey.Location = new System.Drawing.Point(81, 47);
+            this.lblKey.Name = "lblKey";
+            this.lblKey.Size = new System.Drawing.Size(28, 15);
+            this.lblKey.TabIndex = 9;
+            this.lblKey.Text = "Key";
+            // 
+            // txtKey
+            // 
+            this.txtKey.Location = new System.Drawing.Point(135, 44);
+            this.txtKey.Name = "txtKey";
+            this.txtKey.Size = new System.Drawing.Size(286, 23);
+            this.txtKey.TabIndex = 2;
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.ForeColor = System.Drawing.Color.Blue;
+            this.btnCopy.Location = new System.Drawing.Point(427, 106);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(23, 23);
+            this.btnCopy.TabIndex = 11;
+            this.btnCopy.Text = "C";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // MainFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(530, 145);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.lblKey);
+            this.Controls.Add(this.txtKey);
+            this.Controls.Add(this.lblInput);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.btnEncrypt);
+            this.Font = new System.Drawing.Font("Constantia", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MainFrm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "101Encryption";
+            this.Load += new System.EventHandler(this.MainFrm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.Label lblInput;
+        private System.Windows.Forms.Label lblKey;
+        private System.Windows.Forms.TextBox txtKey;
+        private System.Windows.Forms.Button btnCopy;
+    }
+}
+
---
+++ 101Encryption/MainFrm.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace _101Encryption
+{
+    public partial class MainFrm : Form
+    {
+        string[] table = new string[26];
+        char[] charInput = new char[] { };
+
+        public MainFrm()
+        {
+            InitializeComponent();
+        }
+
+        private void Encrypt()
+        {
+            int Key = 0;
+
+            Key = int.Parse(txtKey.Text);
+
+            foreach (char c in charInput)
+            {
+                if (c == ' ')
+                {
+                    txtOutput.Text += " ";
+                }
+                else
+                {
+                    for (int i = 0; i < 26; i++)
+                    {
+                        if (c.ToString() == table[i])
+                        {
+                            if ((i + Key) > 25)
+                            {
+                                txtOutput.Text += table[Key - 1];
+                                break;
+                            }
+                            else
+                            {
+                                txtOutput.Text += table[i + Key];
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void Decrypt()
+        {
+            int Key = 0;
+            Key = int.Parse(txtKey.Text);
+
+            foreach (char c in charInput)
+            {
+                if (c == ' ')
+                {
+                    txtOutput.Text += " ";
+                }
+                else
+                {
+                    for (int i = 0; i < 26; i++)
+                    {
+                        if (c.ToString() == table[i])
+                        {
+
+                            if (i + Key > 25)
+                            {
+                                int NewKey = Key - i;
+                                txtOutput.Text += table[i - NewKey];
+                            }
+                            else
+                            {
+                                txtOutput.Text += table[i - Key];
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void MainFrm_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 26; i++)
+            {
+                table[i] = System.Text.Encoding.Default.GetString(new byte[1] { Convert.ToByte(i + 97) });
+            }
+        }
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            txtOutput.Text = "";
+
+            charInput = txtInput.Text.ToCharArray();
+
+            if (txtInput.Text == "" || txtKey.Text == "")
+            {
+                MessageBox.Show("Please enter some data and a Key to Encrypt.", "101Encryption - Error", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
+            }
+            else
+            {
+                Encrypt();
+            }
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            txtOutput.Text = "";
+
+            charInput = txtInput.Text.ToCharArray();
+
+            if (txtInput.Text == "" || txtKey.Text == "")
+            {
+                MessageBox.Show("Please enter some data and a Key to Decrypt.", "101Encryption - Error", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
+            }
+            else
+            {
+                Decrypt();
+            }
+        }
+
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(txtOutput.Text);
+        }
+    }
+}
